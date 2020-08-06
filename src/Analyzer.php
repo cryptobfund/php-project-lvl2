5be18c1217e0a67447f3fc47fef7c5a830c1f49c
cryptobfund <cryptobfund@gmail.com>
2020-08-06 19:56:45 +0300

    0.0.2

---
+++ src/Analyzer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Gendiff\Analyzer;
+
+function genDiff($format, $firstFilePath, $secondFilePath)
+{
+    if (file_exists($firstFilePath)) {
+        $firstContent = json_decode(file_get_contents($firstFilePath), true);
+    } else {
+        $firstContent = json_decode(file_get_contents(__DIR__ . '/../'), true);
+    }
+    if (file_exists($secondFilePath)) {
+        $secondContent = json_decode(file_get_contents($secondFilePath), true);
+    } else {
+        $secondContent = json_decode(file_get_contents(__DIR__ . '/../'), true);
+    }
+
+    $keysDeleted = array_diff_key($firstContent, $secondContent);
+    $keysAdded = array_diff_key($secondContent, $firstContent);
+    $keysUnchanged = array_intersect_assoc($firstContent, $secondContent);
+    $keysMerge = array_merge($keysDeleted, $keysAdded, $keysUnchanged);
+    $KeysChanged = array_merge_recursive(array_diff_key($firstContent, $keysMerge), array_diff_key($secondContent, $keysMerge));
+
+    $result = "{\n";
+    foreach ($keysUnchanged as $key => $value) {
+        $result = $result . "    {$key}: {$value}\n";
+    }
+    foreach ($keysDeleted as $key => $value) {
+        $result = $result . "  - {$key}: {$value}\n";
+    }
+    foreach ($keysAdded as $key => $value) {
+        $result = $result . "  + {$key}: {$value}\n";
+    }
+    foreach ($KeysChanged as $key => $value) {
+        $result = $result . "  - {$key}: {$value[0]}\n  + {$key}: {$value[1]}\n";
+    }
+    return $result . "}";
+}
--- src/Starter.php
+++ src/Starter.php
@@ -2,6 +2,7 @@
 
 namespace Gendiff\Starter;
 use Docopt;
+use function Gendiff\Analyzer\genDiff;
 
 function run()
 {
@@ -11,15 +12,22 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: pretty]
 
 DOC;
 
     $args = Docopt::handle($doc, array('version'=>'gendiff 0.0.1'));
-    foreach ($args as $k=>$v) {
-        echo $k . ': ' . json_encode($v) . PHP_EOL;
-    }
+    $format = $args['--format'];
+    $firstFilePath = $args['<firstFile>'];
+    $secondFilePath = $args['<secondFile>'];
+
+   // foreach ($args as $k=>$v) {
+   //     echo $k . ': ' . json_encode($v) . PHP_EOL;
+    //}
+    print_r(genDiff($format, $firstFilePath, $secondFilePath));
 }
