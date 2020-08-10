10bc317578c829343fac43f319583222b782504b
cryptobfund <cryptobfund@gmail.com>
2020-08-10 21:09:17 +0300

    0.1.0 last step

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -8,80 +8,41 @@ use function Gendiff\Analyzer\genDiff;
 
 class UserTest extends TestCase
 {
-    private string $expected = <<<DOC
-{
-    host: hexlet.io
-  - timeout: 50
-  + timeout: 20
-  - proxy: 123.234.53.22
-  + verbose: true
-}
-DOC;
-
-    public function testJsonPretty()
-    {
-        $pathBefore = __DIR__ . "/fixtures/before.json";
-        $pathAfter = __DIR__ . "/fixtures/after.json";
-        $this->assertEquals($this->expected, genDiff($pathBefore, $pathAfter, "pretty"));
-    }
+    private string $path = __DIR__ . "/fixtures/";
+    private string $beforeNestedJson = __DIR__ . "/fixtures/beforeNested.json";
+    private string $afterNestedJson = __DIR__ . "/fixtures/afterNested.json";
+    private string $beforeYaml = __DIR__ . "/fixtures/before.yaml";
+    private string $afterYaml = __DIR__ . "/fixtures/after.yaml";
 
     public function testYamlPretty()
     {
-        $pathBefore = __DIR__ . "/fixtures/before.yaml";
-        $pathAfter = __DIR__ . "/fixtures/after.yaml";
-        $this->assertEquals($this->expected, genDiff($pathBefore, $pathAfter, "pretty"));
+        $this->assertEquals(
+            file_get_contents($this->path . "prettyFlatExpected"),
+            genDiff($this->beforeYaml, $this->afterYaml, 'pretty')
+        );
     }
-
-    private string $expectedNested = <<<DOC
-{
-    common: {
-        setting1: Value 1
-      - setting2: 200
-        setting3: true
-      - setting6: {
-            key: value
-        }
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-    }
-  - group2: {
-        abc: 12345
-    }
-  + group3: {
-        fee: 100500
-    }
-}
-DOC;
 
     public function testJsonNestedPretty()
     {
-        $pathBefore = __DIR__ . "/fixtures/beforeNested.json";
-        $pathAfter = __DIR__ . "/fixtures/afterNested.json";
-        $this->assertEquals($this->expectedNested, genDiff($pathBefore, $pathAfter, 'pretty'));
+        $this->assertEquals(
+            file_get_contents($this->path . "prettyExpected"),
+            genDiff($this->beforeNestedJson, $this->afterNestedJson, 'pretty')
+        );
     }
-
-    private string $expectedNestedPlain = <<<DOC
-Property 'common.setting2' was removed
-Property 'common.setting6' was removed
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: 'complex value'
-Property 'group1.baz' was changed. From 'bas' to 'bars'
-Property 'group2' was removed
-Property 'group3' was added with value: 'complex value'
-
-DOC;
 
     public function testJsonNestedPlain()
     {
-        $pathBefore = __DIR__ . "/fixtures/beforeNested.json";
-        $pathAfter = __DIR__ . "/fixtures/afterNested.json";
-        $this->assertEquals($this->expectedNestedPlain, genDiff($pathBefore, $pathAfter, 'plain'));
+        $this->assertEquals(
+            file_get_contents($this->path . "plainExpected"),
+            genDiff($this->beforeNestedJson, $this->afterNestedJson, 'plain')
+        );
+    }
+
+    public function testJsonNestedJson()
+    {
+        $this->assertEquals(
+            file_get_contents($this->path . "jsonExpected"),
+            genDiff($this->beforeNestedJson, $this->afterNestedJson, 'json')
+        );
     }
 }
